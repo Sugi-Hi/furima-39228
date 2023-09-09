da0c7ddd424e24b113fbc6f388ee513bdbf775e5
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-09-09 21:31:59 +0900

    ユーザーに必要なコントローラー機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :access_index , except: [:index, :show]
 
+  def index
+  end
+
+
+
+  def access_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+
+  validates :item_name, presence: true
+
+  belongs_to :user
+  has_one :order
 end
