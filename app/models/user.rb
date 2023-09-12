556ab5756a1a6f5dc07ecab8651e96a3ea159bd1
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-09-12 14:27:34 +0900

    新規登録でのエラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,34 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+         #, :validatable
 
-  validates :nickname          , presence: true
-  # validates :email             , presence: true
-  # validates :encrypted_password, presence: true
-  validates :last_name         , presence: true
-  validates :first_name        , presence: true
-  validates :last_name_kana    , presence: true
-  validates :first_name_kana   , presence: true
-  validates :birth_date        , presence: true
+  # validates :nickname          , presence: { other_than: false , message: "40文字以下で入力して下さい" } 
+  # データベース上migrationファイルで登録済み
+  # validates :email             , presence: { ohter_than: false ,message: "@を含めてて下さい" }
+  # validates :password          , presence: { ohter_than: false ,message: "英字と数字の両方を含めて下さい" }
+ 
+  # with_options presence: true do
+  validates :nickname,  presence: { less_than_or_equal_to: 40 , message: ":40文字以下で入力して下さい" } 
+  validates :email,     format:   {with: /\A[a-z0-9@]+\z/i,  message: ":@を含め、英数字で入力して下さい"}
+  validates :password,  format:   {with: /\A[a-z0-9]+\z/i,   message: ":英字と数字の両方を含め、6字以上で入力して下さい"}
+  validates :last_name,       format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,   message:":かなカナ漢字で入力して下さい" }         
+  validates :first_name,      format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,   message:":かなカナ漢字で入力して下さい"}  
+  validates :last_name_kana , format: {with: /\A[ァ-ヶー]+\z/,   message:":カナで入力して下さい"}
+  validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/,   message:":カナで入力して下さい"}
+  validates :birth_date,      format: {with: /\A[0-9]+\z/i,   message:":数字を選択して下さい"}
+  # end
+
+
+  
+  # , presence: true
+  # def at_mark
+  #   if email.includes("@")
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
 
 end
--- db/migrate/20230908144855_devise_create_users.rb
+++ db/migrate/20230908144855_devise_create_users.rb
@@ -5,8 +5,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :nickname,           null: false
-      t.string :email,              null: false, unique: true, default: ""
-      t.string :encrypted_password, null: false, unique: true, default: ""
+      t.string :email,              null: false, unique: true , default: ""
+      t.string :encrypted_password, null: false, unique: true , default: ""
 
       ## Recoverable
       t.string :first_name,         null: false
