da86c74fa068b3f5c3e0bb41ab8b3a38d663417f
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-09-17 18:58:33 +0900

    商品の出品に関するログイン・ログアウトでのページ移動

--- Gemfile
+++ Gemfile
@@ -68,3 +68,8 @@ end
 gem 'devise'
 
 gem 'pry-rails'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
+
+gem 'active_hash'
--- db/migrate/20230904101823_create_items.rb
+++ db/migrate/20230904101823_create_items.rb
@@ -1,6 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string      :item_name    , null: false
+      t.integer     :price        , null: false
+      t.text        :explain      , null: false
+      t.integer     :category_id  , null: false
+      t.integer     :state_id     , null: false
+      t.integer     :postfee_id   , null: false
+      t.integer     :area_id      , null: false
+      t.integer     :shipdate_id  , null: false
+      t.integer     :user         , null: false
+      # t.references  :user         , null: false, foreign_key: true
 
       t.timestamps
     end
