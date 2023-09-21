1f8958f3a5161865905f4867a7868e0eb770625a
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-09-21 10:30:33 +0900

    出品された商品詳細ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  # before_action :set_item , except: [:index, :new, :create]
-  before_action :authenticate_user!, except: [:index]
+  before_action :set_item , except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
   # before_aciton :item_params , only: [:new, :create, :edit]
 
   def index
@@ -20,8 +20,8 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  # end
+  def show
+  end
 
   # def edit
   # end
@@ -29,15 +29,15 @@ class ItemsController < ApplicationController
   # def destroy
   # end
 
-  # def order
-  # end
+  def order
+  end
 
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :price, :explain, :category_id, :state_id, :postfee_id, :area_id, :shipdate_id).merge(user_id: current_user.id)
   end
 
-  # def set_item
-  #   @item = Item.find(params[:id])
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
