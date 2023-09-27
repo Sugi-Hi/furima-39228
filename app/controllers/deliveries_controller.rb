8ec187fe1a7b551df7b91d9b3a22b38042d495bc
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-09-27 21:00:44 +0900

    必要な追加設定

---
+++ app/controllers/deliveries_controller.rb
@@ -0,0 +1,11 @@
+class DeliveriesController < ApplicationController
+  # def create
+  #   @order = Order.find(params[:order_id])
+  #   @delivery = @order.delivery.new(deli_params)
+  # end
+
+  # private
+  # def deli_params
+  #   params.require(:delivery).permit(:price).merge(order_id: params[:order_id])
+  # end
+end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,10 @@
 class UserController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def order
+    @user = User.find(params[:id])
+    @order = @user.order.new
+  end
 end
