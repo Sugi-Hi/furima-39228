c84eb8f47584e76a9b257af6f4f192881f0556a1
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-09-27 21:06:49 +0900

    購入・クレジット決済に必要コマンド

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,61 @@
+class OrdersController < ApplicationController
+before_action :authenticate_user!
+before_action :return_signin, only: :index
+before_action :set_item 
+before_action :same_top, only: :index 
+
+
+  def index
+    @order = Order.new
+    @delivery = Delivery.new
+  end
+
+  def create
+    # binding.pry # テストコード用
+    @order = Order.new(order_params)
+
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:token , :post_number, :area_id, :city, :tawn, :build, :tel_number ).merge(user_id: current_user.id , item_id: params[:item_id] , token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+
+  end
+
+  def return_signin
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def same_top
+    if  @item.order != nil || current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
+
+end
