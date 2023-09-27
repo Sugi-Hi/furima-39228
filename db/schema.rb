e0e0b268d8441ad4e6c62955a8f5699e492e980d
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-09-27 20:58:39 +0900

    初期の必要な設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_19_110613) do
+ActiveRecord::Schema.define(version: 2023_09_27_073343) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,36 @@ ActiveRecord::Schema.define(version: 2023_09_19_110613) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "buydata", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_number", null: false
+    t.integer "area_id", null: false
+    t.string "city", null: false
+    t.string "tawn", null: false
+    t.string "build"
+    t.string "tel_number", null: false
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_buydata_on_item_id"
+    t.index ["order_id"], name: "index_buydata_on_order_id"
+    t.index ["user_id"], name: "index_buydata_on_user_id"
+  end
+
+  create_table "deliveries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_number", null: false
+    t.integer "area_id", null: false
+    t.string "city", null: false
+    t.string "tawn", null: false
+    t.string "build"
+    t.string "tel_number", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_deliveries_on_order_id"
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.integer "price", null: false
@@ -49,6 +79,12 @@ ActiveRecord::Schema.define(version: 2023_09_19_110613) do
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "post_number", null: false
+    t.integer "area_id", null: false
+    t.string "city", null: false
+    t.string "tawn", null: false
+    t.string "build"
+    t.string "tel_number", null: false
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -76,6 +112,10 @@ ActiveRecord::Schema.define(version: 2023_09_19_110613) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "buydata", "items"
+  add_foreign_key "buydata", "orders"
+  add_foreign_key "buydata", "users"
+  add_foreign_key "deliveries", "orders"
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
