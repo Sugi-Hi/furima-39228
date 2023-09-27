693812e9704049bbc1d540f7c47ce470d631522a
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-09-27 21:02:22 +0900

    遷移・配置の設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "items#index"  
-  resources :items 
+  resources :items do
+    resources :orders , only: [:index, :create] do
+      resources :deliveries , only: :create
+    end
+    resources :buydata , only: [:index, :create]
+  end
 
-  resources :users, only: :show
+  resources :users
+  resources :cards, only: [:new, :create]
+
 end
