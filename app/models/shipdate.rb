332538bbb3ddd5fb2fd3cf7b647b2679ee7fcc21
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2023-10-01 13:32:26 +0900

    最終課題完了後の修正点

--- app/models/shipdate.rb
+++ app/models/shipdate.rb
@@ -3,7 +3,7 @@ class Shipdate < ActiveHash::Base
     { id: 0, name: '---' },
     { id: 1, name: '1~2日で発送' },
     { id: 2, name: '2~3日で発送' },
-    { id: 3, name: '5~7日で発送' }
+    { id: 3, name: '4~7日で発送' }
 
   ]
 
