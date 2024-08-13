84c6ec96b878d22a66e15f31c78239d9dbaf66aa
Sugi-Hi <sugi-hi2017@zd.ztv.ne.jp>
2024-08-13 13:17:55 +0900

    エラー対処のコメントアウト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,11 +21,11 @@ require("../item_price");
 
 
 // エラー対処1
-let promise = chrome.tabs.sendMessage(tabs[0].id, msg);
-promise.catch((error) => {
-    console.log(error);
-});
-// エラー対処2
-var promise = Promise.reject(new Error('promise!!!'));
+// let promise = chrome.tabs.sendMessage(tabs[0].id, msg);
+// promise.catch((error) => {
+//     console.log(error);
+// });
+// // エラー対処2
+// var promise = Promise.reject(new Error('promise!!!'));
 
-setTimeout(function() {promise.catch(/* 適切なエラーハンドル */);}
+// setTimeout(function() {promise.catch(/* 適切なエラーハンドル */);}
